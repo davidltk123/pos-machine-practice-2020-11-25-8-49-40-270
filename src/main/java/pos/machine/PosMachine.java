2f204bb97c2c1373798c210f220292e8cac9566a
davidltk123 <davidltk123@gmail.com>
2020-11-26 21:08:30 +0800

    [POS OO][David Leung]refactor codes

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -2,30 +2,29 @@ package pos.machine;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
         List<ReceiptItem> receiptItems = new ArrayList<>();
-        receiptItems = generateItemDetails(ItemDataLoader.loadBarcodes());
+        receiptItems = generateReceiptItems(ItemDataLoader.loadBarcodes());
         Receipt receipt = new Receipt();
         return receipt.generateReceipt(receiptItems);
     }
 
-    private List<ReceiptItem> generateItemDetails(List<String> loadBarcodes) {
+    private List<ReceiptItem> generateReceiptItems(List<String> loadBarcodes) {
         List<ReceiptItem> receiptItems = new ArrayList<>();
-
-        //create unique barcordes list
         List<String> uniqueBarcodes = BarcodeHandler.getUniqueBarcodes(loadBarcodes);
+        addReceiptItem(loadBarcodes,uniqueBarcodes,receiptItems);
+        return receiptItems;
+    }
 
-        //calculate occurence and form itemDetails object
+    private void addReceiptItem(List<String> loadBarcodes, List<String> uniqueBarcodes,List<ReceiptItem> receiptItems ){
         uniqueBarcodes.forEach(uniqueBarcode ->{
             int quantity = BarcodeHandler.getBarcodeOccurences(loadBarcodes,uniqueBarcode);
             ItemInfo itemInfo = fetchItemInfoFromDatabase(uniqueBarcode);
             ReceiptItem receiptItem = new ReceiptItem(uniqueBarcode,itemInfo.getName(),itemInfo.getPrice(),(int)quantity);
             receiptItems.add(receiptItem);
         });
-        return receiptItems;
     }
 
     private ItemInfo fetchItemInfoFromDatabase(String loadBarcode) {
